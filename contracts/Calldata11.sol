a7ae9378fd7264c0c6f7ac6ff830b0f6ff6ee5a4
trane7776 <izbasarov.t@edu.rea.ru>
2025-02-23 23:37:09 +0300

    commit

--- contracts/Calldata11.sol
+++ contracts/Calldata11.sol
@@ -3,15 +3,14 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
-
-    function sel() external pure returns(bytes4) {
+    function sel() external pure returns (bytes4) {
         return bytes4(keccak256(bytes("work(uint256[3])")));
     }
 
-    function work(uint[3] calldata _arr) external pure returns(bytes32 _el1) {
+    function work(uint[3] calldata _arr) external pure returns (bytes32 _el1) {
         assembly {
-            _el1:=calldataload(4)
-            //0x0000000000000000000000000000000000000000000000000000000000000001
+            _el1 := calldataload(4)
+            //0x00000000000000000000000000000000000000000000000000000000001
         }
     }
 }
